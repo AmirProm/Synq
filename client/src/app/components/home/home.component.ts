26c3592fccecb280a3a19321707d34bdbd3533ef
AmirPorm <promgrameramir@gmail.com>
2025-12-01 00:57:01 +0330

    add lang text in home

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,18 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
-    MatButtonModule , MatCardModule
+    MatButtonModule, MatCardModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  caht = "Chat made simple.Connect instantly and enjoy every conversation.";
+  explor = "Discover more.Find new people, posts, and ideas effortlessly.";
 }
