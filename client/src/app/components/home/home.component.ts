ee89d4191e536b11d6177aaf890c0b64d8ed2622
AmirPorm <promgrameramir@gmail.com>
2025-12-01 00:13:48 +0330

    home add card

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
-    MatButtonModule
+    MatButtonModule , MatCardModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
