8e0b7f4d2524a42f154e0cf117f6c2c2d2ad434f
AmirPorm <promgrameramir@gmail.com>
2025-12-12 19:07:57 +0330

    refactor(Imember): add Metode GetAll Members

--- backend/api/Interfaces/IMemberRepository.cs
+++ backend/api/Interfaces/IMemberRepository.cs
@@ -4,5 +4,7 @@ namespace api.Interfaces;
 
 public interface IMemberRepository
 {
+    public Task<PagedList<AppUser>> GetAllAsync(PaginationParams paginationParams, CancellationToken cancellationToken);
+
     public Task<MemberDto?> GetByUserNameAsync(string userName, CancellationToken cancellationToken);
 }
