dcedc82e6f4d04086e3ae3705f6af050dcd59180
AmirPorm <promgrameramir@gmail.com>
2025-12-14 22:51:09 +0330

    refactor(post controller): Add controle base

--- backend/api/Controllers/PostController.cs
+++ backend/api/Controllers/PostController.cs
@@ -6,10 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class PostsController : ControllerBase
+public class PostsController: BaseApiController
 {
     private readonly IPostRepository _repo;
 
