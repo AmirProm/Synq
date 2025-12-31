535747ce8de6fd7081489466457e697cebda5c16
AmirPorm <promgrameramir@gmail.com>
2025-12-31 21:02:22 +0330

    test(test client): test client /coomends

--- backend/api/Interfaces/IFollowRepository.cs
+++ backend/api/Interfaces/IFollowRepository.cs
@@ -1,7 +1,7 @@
-namespace api.Interfaces;
+// namespace api.Interfaces;
 
-public interface IFollowRepository
-{
-    public Task<FolowStatus> AddFollowAsync(string? userIdHashed, string targetMemberEmail, CancellationToken cancellationToken);
-    public Task<PagedList<AppUser>?> GetFollowMembersAsync(string userIdHashed, FollowParams followParams, CancellationToken cancellationToken);
-}
+// public interface IFollowRepository
+// {
+//     public Task<FolowStatus> AddFollowAsync(string? userIdHashed, string targetMemberEmail, CancellationToken cancellationToken);
+//     public Task<PagedList<AppUser>?> GetFollowMembersAsync(string userIdHashed, FollowParams followParams, CancellationToken cancellationToken);
+// }
