a41128841b098105da87c79273083b3b8dd5f7e0
AmirPorm <promgrameramir@gmail.com>
2025-12-31 21:51:57 +0330

    refactor(postModelTS): Add ID  in post model

--- client/src/app/models/post.model.ts
+++ client/src/app/models/post.model.ts
@@ -3,14 +3,5 @@ export interface Post {
   userName: string;
   caption: string;
   photoUrl: string;
-  createdAt: string; // ISO string
-}
-
-export interface CreatePostDto {
-  caption: string;
-  photoUrl: string;
-}
-
-export interface UpdatePostDto {
-  caption: string;
+  createdAt: Date;
 }
